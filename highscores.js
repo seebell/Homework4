56958d48e3b461c1ebfa378d3b87d7512405247d
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-04-19 00:00:48 -0400

    updates

---
+++ highscores.js
@@ -0,0 +1,4 @@
+// has to reach out to localstorage using the same key that you put the information
+// convert if from string to array
+// make elements, put the info in it, and put it on the page.
+// addeventlistener to clear button, and on click, clear localstorage
